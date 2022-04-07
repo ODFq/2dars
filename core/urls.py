3d5a13b94bb546cba77cc956f9f6b19d02b499d6
ODFq <83254032+ODFq@users.noreply.github.com>
2022-04-07 20:15:23 +0500

    yangi

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 
 from django.urls import path
-from .views import home
+from .views import *
 urlpatterns = [
     
-    path('',home)
-]
+    path('',home),
+     path('example/',example)
+    ]
+
+
--- core/views.py
+++ core/views.py
@@ -1,6 +1,19 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+   posts = Post.objects.all()
+   context = {
+        'posts':posts}
+   return render(request, 'index.html',context)
+
+
+def example(request):
+    posts = Post.objects.all()
+    context = {
+        'posts':posts
+
+    }
+    return render(request, 'examp.html',context)
